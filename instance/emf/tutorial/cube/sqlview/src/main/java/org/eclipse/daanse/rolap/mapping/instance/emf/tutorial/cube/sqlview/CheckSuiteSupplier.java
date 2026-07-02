71eb39b186b2278673ecf24107cc1f8b0c84d37d
dbulahov <bulahovdenis@gmail.com>
2026-07-02 09:59:30 +0300

    add minimal cube tutorial with sql view as fact Signed-off-by: dbulahov <bulahovdenis@gmail.com>

---
+++ instance/emf/tutorial/cube/sqlview/src/main/java/org/eclipse/daanse/rolap/mapping/instance/emf/tutorial/cube/sqlview/CheckSuiteSupplier.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ */
+package org.eclipse.daanse.rolap.mapping.instance.emf.tutorial.cube.sqlview;
+
+import org.eclipse.daanse.olap.check.model.check.CatalogCheck;
+import org.eclipse.daanse.olap.check.model.check.OlapCheckFactory;
+import org.eclipse.daanse.olap.check.model.check.OlapCheckSuite;
+import org.eclipse.daanse.olap.check.model.check.OlapConnectionCheck;
+import org.eclipse.daanse.olap.check.runtime.api.OlapCheckSuiteSupplier;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Provides a check suite for the database SQL view tutorial.
+ * Checks that the catalog and database schema with SQL view exist and are properly configured.
+ */
+@Component(service = OlapCheckSuiteSupplier.class)
+public class CheckSuiteSupplier implements OlapCheckSuiteSupplier {
+
+    private static final OlapCheckFactory factory = OlapCheckFactory.eINSTANCE;
+
+    @Override
+    public OlapCheckSuite get() {
+
+        // Create catalog check
+        CatalogCheck catalogCheck = factory.createCatalogCheck();
+        catalogCheck.setName("Daanse Tutorial - Database SQL View");
+        catalogCheck.setDescription("Check that catalog 'Daanse Tutorial - Database SQL View' exists with database schema");
+        catalogCheck.setCatalogName("Daanse Tutorial - Database SQL View");
+
+        // Create connection check (uses default connection)
+        OlapConnectionCheck connectionCheck = factory.createOlapConnectionCheck();
+        connectionCheck.setName("Connection Check Daanse Tutorial - Database SQL View");
+        connectionCheck.setDescription("Connection check for Daanse Tutorial - Database SQL View");
+        connectionCheck.getCatalogChecks().add(catalogCheck);
+
+        // Create suite containing the connection check
+        OlapCheckSuite suite = factory.createOlapCheckSuite();
+        suite.setName("Daanse Tutorial - Database SQL View");
+        suite.setDescription("Check suite for the Daanse Tutorial - Database SQL View");
+        suite.getConnectionChecks().add(connectionCheck);
+
+        return suite;
+    }
+}
